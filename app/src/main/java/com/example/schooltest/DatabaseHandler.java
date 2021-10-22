16ff776f38a6894b4a684f8c93775893c031579c
xphil-22 <75225050+xphil-22@users.noreply.github.com>
2021-10-22 15:11:12 +0200

    Added DatabaseHandler

--- app/src/main/java/com/example/schooltest/DatabaseHandler.java
+++ app/src/main/java/com/example/schooltest/DatabaseHandler.java
@@ -1,42 +1,67 @@
 package com.example.schooltest;
 
+import android.content.ContentValues;
+import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 import java.lang.reflect.Array;
-import java.util.Locale;
 
-public class DatabaseHandler {
-    private SQLiteDatabase database;
-    private String DatabaseName;
+public class DatabaseHandler extends SQLiteOpenHelper {
+    //Attributes
+    private static final String TAG = "DbHandler";
 
-    public void create(String DatabaseName){ //DataBaseName must contain .db at the end
-        this.DatabaseName = DatabaseName;
-        database = SQLiteDatabase.openOrCreateDatabase(DatabaseName,null);
-        database.close();
+    private static final String TABLE_NAME = "UserLogin";
+    private static final String col1 = "username";
+    private static final String col2 = "password";
+    public DatabaseHandler(Context context){
+        super(context, TABLE_NAME, null, 1);
     }
 
-    private void openDatabase(){
-        database = SQLiteDatabase.openOrCreateDatabase(DatabaseName, null);
+    @Override
+    public void onCreate(SQLiteDatabase db){
+        String createTable = "CREATE TABLE "+TABLE_NAME+
+                " ("+col1+" VARCHAR(255) ,"+ col2+" VARCHAR(255));";
+
+        try {
+            db.execSQL(createTable);
+        } catch (Exception e) {
+            Log.e("createTableError", String.valueOf(e));
+        }
     }
 
-    public void createUserData(String username, String password){
-        openDatabase();
-        database.execSQL("CREATE TABLE " + "UserLoginData" + "(username TEXT, password TEXT)");
-        database.execSQL("INSERT INTO " + "UserLoginData" + " VALUES(username,password)");
-        database.close();
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1){
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
     }
 
-    public String[] getLoginData(){
-        String[] UserData = new String[2];
-        Cursor cursor = database.rawQuery("SELECT * FROM " + "UserLoginData", null);
-        cursor.moveToFirst();
-        UserData[0] = cursor.getString(0);
-        UserData[1] = cursor.getString(1);
-        cursor.close();
-        database.close();
+    public boolean addUserLoginData(String username, String password){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(col1, username);
+        contentValues.put(col2, password);
 
-        return UserData;
-        //return
+        Log.e(TAG, "addData: Adding "+ username + " and " + password + " to" + TABLE_NAME);
+
+        long result = db.insert(TABLE_NAME, null, contentValues);
+        db.close();
+        //when insertion is incorrect it will return -1 -> false
+        return result != -1;
+    }
+
+    public String[] getUserLoginData(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        String query = "SELECT username,password FROM " + TABLE_NAME;
+        Cursor Raw_data = db.rawQuery(query, null);
+        String[] data = new String[2];
+        Raw_data.moveToFirst();
+        data[0] = Raw_data.getString(0);    
+        data[1] = Raw_data.getString(1);
+        db.close();
+        Raw_data.close();
+        return data; //Returns Login Data Array -> [0] is username [1] is password
     }
 }
