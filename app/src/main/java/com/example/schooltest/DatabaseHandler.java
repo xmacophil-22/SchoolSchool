79170e71060bd6ae6ccea4e84b4fadab47fb32f3
xphil-22 <75225050+xphil-22@users.noreply.github.com>
2021-10-28 12:46:51 +0200

    Added Key and Id fields and set/get methods in Database

--- app/src/main/java/com/example/schooltest/DatabaseHandler.java
+++ app/src/main/java/com/example/schooltest/DatabaseHandler.java
@@ -13,21 +13,24 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     //Attributes
     private static final String TAG = "DbHandler";
 
-    private static final String User_Login_Data = "UserLoginDatabase";
+    private static final String User_Login_Data = "User_Logetttt";
     //private static final String school_Data = "UserLoginDatabase";
-    private static final String col1 = "username";
-    private static final String col2 = "password";
-    private static final String col3 = "authKey";
+    private static final String[] col = {"username", "password", "authKey", "snippetId"};
 
     public DatabaseHandler(Context context){
-        super(context, User_Login_Data, null, 1);
+        super(context, User_Login_Data, null, 12);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db){
         String createTable = "CREATE TABLE "+User_Login_Data+
-                " ("+col1+" VARCHAR(255) ,"+ col2+" VARCHAR(255) ,"+ col3+" VARCHAR(255));";
-                //" ("+col1+" VARCHAR(255) ,"+ col2+" VARCHAR(255));";
+                " ("
+                + col[0] + " VARCHAR(255) ,"
+                + col[1] + " VARCHAR(255) ,"
+                + col[2] + " VARCHAR(255) ,"
+                + col[3] + " VARCHAR(255)"
+                + ");";
+
         try {
             db.execSQL(createTable);
         } catch (Exception e) {
@@ -41,45 +44,55 @@ public class DatabaseHandler extends SQLiteOpenHelper {
         onCreate(db);
     }
 
-    public boolean addUserLoginData(String username, String password){
+    public boolean addUserLoginData(String username, String password, String authKey, String id){
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
-        contentValues.put(col1, username);
-        contentValues.put(col2, password);
-
-        Log.e(TAG, "addData: Adding "+ username + " and " + password + " to" + User_Login_Data);
+        contentValues.put(col[0],username);
+        contentValues.put(col[1], password);
+        contentValues.put(col[2], authKey);
+        contentValues.put(col[3], id);
 
         long result = db.insert(User_Login_Data, null, contentValues);
+
         db.close();
         //when insertion is incorrect it will return -1 -> false
         return result != -1;
     }
 
-    public void addKey(String AuthKey){
-        SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues contentValues = new ContentValues();
-        contentValues.put(col3, AuthKey);
-
-        long result = db.insert(User_Login_Data, null, contentValues);
-    }
-
     public String getKey(){
         SQLiteDatabase db = this.getWritableDatabase();
-        String query = "SELECT authKey FROM " + User_Login_Data;
+        String query = "SELECT "+col[2]+" FROM " + User_Login_Data;
 
         Cursor Raw_data = db.rawQuery(query, null);
 
         Raw_data.moveToFirst();
-        String data = Raw_data.getString(2);
-
+        String data = Raw_data.getString(0);
         db.close();
         Raw_data.close();
-        return data; //Returns Login Data Array -> [0] is username [1] is password
+        return data;
+    }
+
+    public String getId(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        String query = "SELECT "+col[3]+" FROM " + User_Login_Data;
+
+        Cursor Raw_data = db.rawQuery(query, null);
+
+        Raw_data.moveToFirst();
+        String data = Raw_data.getString(0);
+        Log.e("UserKey: ", data);
+        db.close();
+        Raw_data.close();
+        return data;
     }
 
     public String[] getUserLoginData(){
         SQLiteDatabase db = this.getWritableDatabase();
-        String query = "SELECT username,password FROM " + User_Login_Data;
+        String query = "SELECT "
+            +col[0]+
+            ","
+            +col[1]+
+        " FROM " + User_Login_Data;
         Cursor Raw_data = db.rawQuery(query, null);
         String[] data = new String[2];
         Raw_data.moveToFirst();
