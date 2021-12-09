10492d4e71e7019f92edd8e52687f39608ecb260
Maya Kuhlmann <maya.zoe@gmx.de>
2021-12-09 22:26:41 +0100

    Navigation Bar works

--- app/src/main/java/com/example/schooltest/GradesActivity.java
+++ app/src/main/java/com/example/schooltest/GradesActivity.java
@@ -6,11 +6,16 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.GridView;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -37,11 +42,12 @@ public class GradesActivity extends AppCompatActivity {
     PopUpError popUpError;
     RequestQueue requestQueue;
     Snippet snippet;
-
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grades);
+
+
         Bundle bundle = getIntent().getExtras();
         id = bundle.getString("id");
         key = bundle.getString("key");
@@ -53,44 +59,12 @@ public class GradesActivity extends AppCompatActivity {
         usernameTV.setText(databaseHandler.getUserLoginData()[0]);
         untisLoginPopUp = new UntisLoginPopUp(this, requestQueue, key);
 
-        /*ArrayList<Integer> c1w = new ArrayList<>();
-        c1w.add(10);
-        c1w.add(12);
-        ArrayList<Integer> c2w = new ArrayList<>();
-        c2w.add(15);
-        ArrayList<Integer> c3w = new ArrayList<>();
-        c3w.add(15);
-        ArrayList<Integer> c4w = new ArrayList<>();
-        c4w.add(15);
-        ArrayList<Integer> c1s = new ArrayList<>();
-        c1s.add(15);
-        ArrayList<Integer> c2s = new ArrayList<>();
-        c2s.add(15);
-        ArrayList<Integer> c3s = new ArrayList<>();
-        c3s.add(15);
-        ArrayList<Integer> c4s = new ArrayList<>();
-        c4s.add(15);
-
-
-
-        int color = Color.rgb(161,84,84);
-        int color2 = Color.rgb(0,255,0);
-        Subject subject2 = new Subject("Mathe", color2, "Malenica", 60,40, c1w,c2w,c3w,c4w,c1s,c2s,c3s,c4s,View.VISIBLE);
-        Subject subject = new Subject("Deutsch", color ,"Hilgarth", 50,50, c1w,c2w,c3w,c4w,c1s,c2s,c3s,c4s,View.VISIBLE);
-        Subject subjec1 = new Subject("Deutsch", color ,"Hilgarth", 50,50, c1w,c2w,c3w,c4w,c1s,c2s,c3s,c4s,View.VISIBLE);
-        Subject subjec3 = new Subject("Deutsch", color ,"Hilgarth", 50,50, c1w,c2w,c3w,c4w,c1s,c2s,c3s,c4s,View.VISIBLE);
-        Subject subjec4 = new Subject("Deutsch", color ,"Hilgarth", 50,50, c1w,c2w,c3w,c4w,c1s,c2s,c3s,c4s,View.VISIBLE);*/
         Subject subject00 = new Subject("", 0 ,"", 1,1, new ArrayList<Integer>(), new ArrayList<Integer>(),
                 new ArrayList<Integer>(),new ArrayList<Integer>(),new ArrayList<Integer>(),new ArrayList<Integer>(),
                 new ArrayList<Integer>(),new ArrayList<Integer>(),View.INVISIBLE);
         mySubjects = new ArrayList<>();
         mySubjects.add(subject00);
         mySubjects.add(subject00);
-        /*mySubjects.add(subject);
-        mySubjects.add(subject2);
-        mySubjects.add(subjec1);
-        mySubjects.add(subjec3);
-        mySubjects.add(subjec4);*/
         myGrid = (RecyclerView) findViewById(R.id.gridListRV);
         myGrid.setLayoutManager(new GridLayoutManager(this, 2));
 
---
+++ app/src/main/java/com/example/schooltest/Main.java
@@ -0,0 +1,65 @@
+package com.example.schooltest;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class Main extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawer;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawer,toolbar,R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            /*case R.id.nav_grades:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new GradesActivity()).commit();
+                break;
+            case R.id.nav_food:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new GradesActivity()).commit();
+                break;*/
+            case R.id.nav_logout:
+                ActivityHandler.switchActivity(this, MainActivity.class, "","");
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }
+        else{
+        super.onBackPressed();
+        }
+    }
+}
